5f92731e1242f7583c06c845be78257cc96e576c
agacia1805 <agacia1805@gmail.com>
2024-01-31 21:11:56 +0000

    (feat) add initial logic for timer app and structure page with components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,142 @@
-import Image from "next/image";
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import Input from '@/app/ui/input';
+import Button from '@/app/ui/button';
+
+type TimeRemaining = {
+  hours: number;
+  minutes: number;
+  seconds: number;
+};
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  const [targetTime, setTargetTime] = useState<Date | null>(null);
+  const [paused, setPaused] = useState(false);
+  const [timeRemaining, setTimeRemaining] = useState<TimeRemaining>({
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+  const calculateTimeLeft = () => {
+    if (targetTime) {
+      const now = new Date();
+      let difference = +targetTime - +now;
+
+      if (difference > 0) {
+        let hours = Math.floor(difference / (1000 * 60 * 60));
+        let minutes = Math.floor((difference / (1000 * 60)) % 60);
+        let seconds = Math.floor((difference / 1000) % 60);
+
+        return { hours, minutes, seconds };
+      }
+    }
+    return null;
+  };
+
+  const startTimer = (hours: number, minutes: number, seconds: number) => {
+    setPaused(false);
+
+    let additionalHours = Math.floor(minutes / 60);
+    hours += additionalHours;
+    minutes = minutes % 60;
+
+    const now = new Date();
+    const target = new Date(
+      now.getTime() + hours * 3600000 + minutes * 60000 + seconds * 1000
+    );
+    setTargetTime(target);
+
+    setTimeRemaining({
+      hours: hours + additionalHours,
+      minutes: minutes,
+      seconds: seconds,
+    });
+  };
+
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+    if (targetTime && !paused) {
+      timer = setInterval(() => {
+        const timeLeft = calculateTimeLeft();
+        if (timeLeft) {
+          setTimeRemaining(timeLeft);
+        } else {
+          clearInterval(timer);
+        }
+      }, 1000);
+    }
+
+    return () => clearInterval(timer);
+  }, [targetTime, paused]);
+
+  const pauseResumeTimer = () => {
+    setPaused(!paused);
+  };
+
+  const resetTimer = () => {
+    setTargetTime(null);
+    setPaused(false);
+    setTimeRemaining({ hours: 0, minutes: 0, seconds: 0 });
+  };
+
+  const handleTimeInputChange = (value: string, type: 'hours' | 'minutes' | 'seconds', maxInput: number) => {
+    const numValue = parseInt(value, 10) || 0;
+
+    if (numValue > maxInput) {
+      return;
+    }
+
+    setTimeRemaining({
+      ...timeRemaining,
+      [type]: numValue,
+    });
+  };
+
+  return (
+    <div className='flex flex-col items-center justify-center gap-16'>
+      <div className='flex flex-col gap-8 md:flex-row'>
+        <Input
+          name='hours'
+          handleOnChange={(e) =>
+            handleTimeInputChange(e.target.value, 'hours', 99)
+          }
+          remainingTime={timeRemaining.hours}
+        />
+        <Input
+          name='minutes'
+          handleOnChange={(e) =>
+            handleTimeInputChange(e.target.value, 'minutes', 60)
+          }
+          remainingTime={timeRemaining.minutes}
+        />
+        <Input
+          name='seconds'
+          handleOnChange={(e) =>
+            handleTimeInputChange(e.target.value, 'seconds', 60)
+          }
+          remainingTime={timeRemaining.seconds}
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <Button
+        name='Start button'
+        onClick={() =>
+          startTimer(
+            timeRemaining.hours,
+            timeRemaining.minutes,
+            timeRemaining.seconds
+          )
+        }
+      >
+        Start
+      </Button>
+      <Button name='Pause resume button' onClick={pauseResumeTimer}>
+        {paused ? 'Resume' : 'Pause'}
+      </Button>
+      <Button name='Pause resume button' onClick={resetTimer}>
+        Reset
+      </Button>
+    </div>
   );
 }
