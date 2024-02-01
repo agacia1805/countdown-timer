0529d9959f6acf7b5561ddc404b4fe3a5e74dcf8
agacia1805 <agacia1805@gmail.com>
2024-02-01 21:15:13 +0000

    (chore) remove logic from component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,103 +3,21 @@
 import React, { useEffect, useState } from 'react';
 import Input from '@/app/ui/input';
 import Button from '@/app/ui/button';
-
-type TimeRemaining = {
-  hours: number;
-  minutes: number;
-  seconds: number;
-};
+import useCountdownTimer from './hooks/useCountdownTimer';
 
 export default function Home() {
-  const [targetTime, setTargetTime] = useState<Date | null>(null);
-  const [paused, setPaused] = useState(false);
-  const [timeRemaining, setTimeRemaining] = useState<TimeRemaining>({
-    hours: 0,
-    minutes: 0,
-    seconds: 0,
-  });
-
-  const calculateTimeLeft = () => {
-    if (targetTime) {
-      const now = new Date();
-      let difference = +targetTime - +now;
-
-      if (difference > 0) {
-        let hours = Math.floor(difference / (1000 * 60 * 60));
-        let minutes = Math.floor((difference / (1000 * 60)) % 60);
-        let seconds = Math.floor((difference / 1000) % 60);
-
-        return { hours, minutes, seconds };
-      }
-    }
-    return null;
-  };
-
-  const startTimer = (hours: number, minutes: number, seconds: number) => {
-    setPaused(false);
-
-    let additionalHours = Math.floor(minutes / 60);
-    hours += additionalHours;
-    minutes = minutes % 60;
-
-    const now = new Date();
-    const target = new Date(
-      now.getTime() + hours * 3600000 + minutes * 60000 + seconds * 1000
-    );
-    setTargetTime(target);
-
-    setTimeRemaining({
-      hours: hours + additionalHours,
-      minutes: minutes,
-      seconds: seconds,
-    });
-  };
-
-  useEffect(() => {
-    let timer: NodeJS.Timeout;
-    if (targetTime && !paused) {
-      timer = setInterval(() => {
-        const timeLeft = calculateTimeLeft();
-        if (timeLeft) {
-          setTimeRemaining(timeLeft);
-        } else {
-          clearInterval(timer);
-        }
-      }, 1000);
-    }
-
-    return () => clearInterval(timer);
-  }, [targetTime, paused]);
-
-  const pauseResumeTimer = () => {
-    setPaused(!paused);
-  };
-
-  const resetTimer = () => {
-    setTargetTime(null);
-    setPaused(false);
-    setTimeRemaining({ hours: 0, minutes: 0, seconds: 0 });
-  };
-
-  const handleTimeInputChange = (
-    value: string,
-    type: 'hours' | 'minutes' | 'seconds',
-    maxInput: number
-  ) => {
-    const numValue = parseInt(value, 10) || 0;
-
-    if (numValue > maxInput) {
-      return;
-    }
-
-    setTimeRemaining({
-      ...timeRemaining,
-      [type]: numValue,
-    });
-  };
+  const {
+    timeRemaining,
+    startTimer,
+    pauseResumeTimer,
+    resetTimer,
+    paused,
+    targetTime,
+    handleTimeInputChange,
+  } = useCountdownTimer({ hours: 0, minutes: 0, seconds: 0 });
 
   return (
-    <div className='flex flex-col items-center justify-center gap-16'>
+    <div className='flex flex-col items-center justify-center gap-24'>
       <div className='flex flex-col gap-12 lg:flex-row'>
         <Input
           name='hours'
