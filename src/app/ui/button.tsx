77bfe872fbdb4c4bc45caa3424582e8ab7ebc1b0
agacia1805 <agacia1805@gmail.com>
2024-02-01 18:46:28 +0000

    (chore) update styles and clear globals css file

--- src/app/ui/button.tsx
+++ src/app/ui/button.tsx
@@ -3,17 +3,28 @@ import * as React from 'react';
 type ButtonProps = {
   children: React.ReactNode;
   className?: string;
+  disabled?: boolean;
   onClick: () => void;
   name: string;
 };
 
-export default function Button({ name, onClick, children }: ButtonProps) {
+export default function Button({
+  name,
+  onClick,
+  children,
+  disabled,
+}: ButtonProps) {
   return (
     <button
       type='button'
       name={name}
+      disabled={disabled}
       onClick={onClick}
-      className='rounded-lg border px-8 py-2'
+      className={`rounded-lg border px-8 py-2 text-lg font-medium shadow-lg transition duration-150 ease-in-out ${
+        !disabled
+          ? 'hover:-translate-y-1 hover:shadow-xl active:translate-y-0 active:shadow-sm'
+          : 'cursor-not-allowed opacity-50'
+      }`}
     >
       {children}
     </button>
